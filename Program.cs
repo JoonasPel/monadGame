089b72a263bb1b7a41838678419fa20eb96ef95d
Joonas Pelttari <joonas.pelttari@gmail.com>
2024-01-22 13:15:28 +0200

    Refactor

--- Program.cs
+++ Program.cs
@@ -20,50 +20,73 @@ public class Program
     string? body = await app.CreateGame();
     if (body is null) app.CloseProgram();
     _entityID = GetEntityID(body);
-    var websocket = new ClientWebSocket();
-    await websocket.ConnectAsync(new Uri(
-      $"ws://goldrush.monad.fi/backend/{_playerToken}"), CancellationToken.None);
-
-    if (websocket.State == WebSocketState.Open)
+    ClientWebSocket websocket = await ConnectWebsocket();
+    bool subscribeSuccess = await SubscribeToGame(websocket);
+    if (!subscribeSuccess) app.CloseProgram();
+    Action action = new Action();
+    while (true)
     {
-      object payload = new { id = _entityID };
-      string message = JsonConvert.SerializeObject(
-        new object[] { "sub-game", payload });
+      JObject? gameState = await WaitForNextGameTick(websocket);
+      object payload = action.GenerateAction(gameState);
+      Thread.Sleep(800);
+      string message = CreateMessage(payload);
       await SendMessage(websocket, message);
-    }
-
-    while (websocket.State == WebSocketState.Open)
-    {
-      byte[] buffer = new byte[1024];
-      Console.WriteLine("alive");
-
-      WebSocketReceiveResult result = await websocket.ReceiveAsync(
-        new ArraySegment<byte>(buffer), CancellationToken.None);
-      if (result.MessageType == WebSocketMessageType.Text)
-      {
-        string receivedMsg = Encoding.UTF8.GetString(buffer, 0, result.Count);
-        dynamic? dataObject = JsonConvert.DeserializeObject(receivedMsg);
-        JObject? gameState = JsonConvert.DeserializeObject<JObject>(
-          dataObject[1]["gameState"]);
-        Console.WriteLine(gameState["player"]["position"]);
-      }
-
-      object data = new
-      {
-        gameId = _entityID,
-        payload = new { action = "rotate", rotation = 45 }
-      };
-      string message = JsonConvert.SerializeObject(
-        new object[] { "run-command", data });
-      await SendMessage(websocket, message);
-
-      Thread.Sleep(1000);
+      // TODO Detect game win or something and close program.
     }
 
     await websocket.CloseAsync(
       WebSocketCloseStatus.NormalClosure, "Done", CancellationToken.None);
   }
 
+  private static string CreateMessage(object payload)
+  {
+    object data = new
+    {
+      gameId = _entityID,
+      payload,
+    };
+    return JsonConvert.SerializeObject(new object[] { "run-command", data });
+  }
+
+  private static async Task<JObject?> WaitForNextGameTick(ClientWebSocket websocket)
+  {
+    while (websocket.State == WebSocketState.Open)
+    {
+      byte[] buffer = new byte[1024];
+      WebSocketReceiveResult result = await websocket.ReceiveAsync(
+        new ArraySegment<byte>(buffer), CancellationToken.None);
+      if (result.MessageType == WebSocketMessageType.Text)
+      {
+        string receivedMsg = Encoding.UTF8.GetString(buffer, 0, result.Count);
+        dynamic? dataObject = JsonConvert.DeserializeObject(receivedMsg);
+        string temp = dataObject[1]["gameState"];
+        JObject gameState = JsonConvert.DeserializeObject<JObject>(temp);
+        return gameState;
+      }
+    }
+    return null;
+  }
+
+  private static async Task<bool> SubscribeToGame(ClientWebSocket websocket)
+  {
+    if (websocket.State == WebSocketState.Open)
+    {
+      object payload = new { id = _entityID };
+      string message = JsonConvert.SerializeObject(
+        new object[] { "sub-game", payload });
+      await SendMessage(websocket, message);
+      return true;
+    }
+    return false;
+  }
+
+  private static async Task<ClientWebSocket> ConnectWebsocket()
+  {
+    var websocket = new ClientWebSocket();
+    await websocket.ConnectAsync(new Uri(
+      $"ws://goldrush.monad.fi/backend/{_playerToken}"), CancellationToken.None);
+    return websocket;
+  }
 
   private static async Task SendMessage(ClientWebSocket webSocket, string message)
   {
